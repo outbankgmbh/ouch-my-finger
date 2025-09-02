29b9265d9bc9a858c520de38d0e541f0311ec1b8
Markus Schicker <markus@outbankapp.com>
2025-09-02 19:15:47 +0200

    Example for @omit not working

--- graphile.config.ts
+++ graphile.config.ts
@@ -1,60 +1,45 @@
 import "graphile-config";
 
 import { makePgService } from "@dataplan/pg/adaptors/pg";
-import AmberPreset from "postgraphile/presets/amber";
 import { makeV4Preset } from "postgraphile/presets/v4";
-import { makePgSmartTagsFromFilePlugin } from "postgraphile/utils";
-import { PostGraphileConnectionFilterPreset } from "postgraphile-plugin-connection-filter";
-import { PgAggregatesPreset } from "@graphile/pg-aggregates";
-import { PgManyToManyPreset } from "@graphile-contrib/pg-many-to-many";
-// import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
-import PersistedPlugin from "@grafserv/persisted";
-import { PgOmitArchivedPlugin } from "@graphile-contrib/pg-omit-archived";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
+import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
+import { PgAggregatesPreset } from "@graphile/pg-aggregates";
+import { PostGraphileConnectionFilterPreset } from "postgraphile-plugin-connection-filter";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// For configuration file details, see: https://postgraphile.org/postgraphile/next/config
-
-const TagsFilePlugin = makePgSmartTagsFromFilePlugin(`${__dirname}/tags.json5`);
-
+const isDevelopment = true;
 const preset: GraphileConfig.Preset = {
   extends: [
-    AmberPreset.default ?? AmberPreset,
-    makeV4Preset({
-      /* Enter your V4 options here */
-      graphiql: true,
-      graphiqlRoute: "/",
-    }),
-    PostGraphileConnectionFilterPreset,
-    PgManyToManyPreset,
+    PostGraphileAmberPreset,
+    PgSimplifyInflectionPreset,
     PgAggregatesPreset,
-    // PgSimplifyInflectionPreset
+    PostGraphileConnectionFilterPreset,
+    makeV4Preset({
+      bodySizeLimit: 100 * 1024 * 1024,
+      subscriptions: false,
+      retryOnInitFail: true,
+      dynamicJson: true,
+      watchPg: isDevelopment,
+      setofFunctionsContainNulls: false,
+      ignoreRBAC: false,
+      ignoreIndexes: false,
+      graphiql: isDevelopment,
+      showErrorStack: isDevelopment ? "json" : false,
+      extendedErrors: isDevelopment
+        ? ["hint", "detail", "errcode"]
+        : ["errcode"],
+      enhanceGraphiql: isDevelopment,
+      allowExplain: true,
+    }),
   ],
-  plugins: [PersistedPlugin.default, PgOmitArchivedPlugin, TagsFilePlugin],
   pgServices: [
     makePgService({
-      // Database connection string:
       connectionString: process.env.DATABASE_URL,
-      superuserConnectionString:
-        process.env.SUPERUSER_DATABASE_URL ?? process.env.DATABASE_URL,
-      // List of schemas to expose:
-      schemas: process.env.DATABASE_SCHEMAS?.split(",") ?? ["public"],
-      // Enable LISTEN/NOTIFY:
-      pubsub: true,
+      schemas: ["public"],
     }),
   ],
-  grafserv: {
-    port: 5678,
-    websockets: true,
-    allowUnpersistedOperation: true,
-    watch: true,
-  },
-  grafast: {
-    explain: true,
-  },
 };
 
 export default preset;
--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
--- Create your database schema here
+CREATE TABLE table_that_should_be_hidden (
+    foreign_id SERIAL PRIMARY KEY,
+    data VARCHAR(100) NOT NULL
+);
+COMMENT ON TABLE table_that_should_be_hidden IS E'@omit all\n';
+
+CREATE TABLE table_that_is_hidden(
+    foreign_id SERIAL PRIMARY KEY,
+    data VARCHAR(100) NOT NULL
+);
+COMMENT ON TABLE table_that_is_hidden IS E'@behavior -*\n';
+
